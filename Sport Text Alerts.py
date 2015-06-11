ff773ab0117b4fb24222e22e52ae2c2e93ec048d
spdydve <david.kennedy48@gmail.com>
2015-06-11 11:17:21 -0400

    Test update
    Test

--- Sport Text Alerts.py
+++ Sport Text Alerts.py
@@ -8,8 +8,9 @@ class MyHTMLParser(HTMLParser):
         self.data_list = []
         self.i = 0
         self.j = 0
-    def handle_starttag(self, tag):
-        print 'need to fill in'
+
+    def handle_data(self, data):
+        print "Encountered some data  :", data
     def handle_data(self, data):
         if data.lower() == 'scoring summary':
             print "Scoring Summary"
@@ -19,20 +20,21 @@ class MyHTMLParser(HTMLParser):
             print "Team Stat Comparison"
             self.j = len(self.data_list)
             print self.j
-            
+
         self.data_list.append(data)
     def get_data(self):
         score_data = self.data_list[self.i:self.j]
         return score_data
-        
+
 def data_collector(html):
     parser = MyHTMLParser()
     parser.feed(html)
     return parser.get_data()
-                                
 
-page = urllib2.urlopen('http://scores.espn.go.com/ncf/boxscore?gameId=400610325')
+
+page = urllib2.urlopen('https://rdcrnqa.epi.usf.edu//rdnwebapp/Forms/05VCRC/5532/RecurringMedications.aspx?EventScheduleId=12658&RdcrnProtocolId=5532&ProtocolId=363&creguid=1697&trimester=4')
 html = page.read()
 
 print data_collector(html)
 
+# Hello
